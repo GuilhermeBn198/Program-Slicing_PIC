cdaa77dded45176eeba2236ff71788ee760934a4
Guilherme Bernardo <bguilherme51@gmail.com>
2023-08-10 15:51:37 -0400

    tests finished

--- tests/loop_tests/loop_gcnr2008/gcnr2008.c
+++ tests/loop_tests/loop_gcnr2008/gcnr2008.c
@@ -1,7 +1,22 @@
 // Source: Bhargav S. Gulavani, Supratik Chakraborty, Aditya V. Nori, Sriram
 // K. Rajamani: "Automatically Refining Abstract Interpretations", TACAS 2008.
 
-#include "assert.h"
+extern void abort(void); 
+//#include <assert.h>
+void reach_error() { assert(0); }
+extern void abort(void); 
+void assume_abort_if_not(int cond) { 
+  if(!cond) {abort();}
+}
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+      ERROR: {reach_error();abort();}
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
+#define LARGE_INT 1000000
+
 
 int main() {
     int x,y,z,w;
